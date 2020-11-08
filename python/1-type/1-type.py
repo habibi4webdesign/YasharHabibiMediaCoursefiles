5a94b1dcead20c853866e59f32350c9aebf92127
YasharHabibi <habibi4webdesign@gmail.com>
2020-11-08 12:13:00 +0330

    python files

---
+++ python/1-type/1-type.py
@@ -0,0 +1,16 @@
+# در پایتون "type" آموزش فانکشن
+# پایتون 5 نوع داده استاندارد داره
+# Numbers
+# String
+# List
+# Tuple
+# Dictionary
+# یک متغیر میتونه انواع متفاوتی رو بگیره
+
+x = 42
+print(x)
+print(type(x))
+
+x = "ye reshte"
+print(x)
+print(type(x))
---
+++ python/4-len/4-len.py
@@ -0,0 +1,11 @@
+# در پایتون "len" آموزش فانکشن
+
+
+list_var = [1, 2, 3, 4, 5]
+str_var = "Hello World"
+dict_var = {"a": 100, "b": 200}
+
+print(len(list_var))
+print(len(str_var))
+print(len(dict_var))
+
