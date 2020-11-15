d79f115eb7a176d29d9b0de4e88a19c0ff69652a
YasharHabibi <habibi4webdesign@gmail.com>
2020-11-15 20:26:45 +0330

    python string

---
+++ javascript/3-output/myCode.js
@@ -0,0 +1,14 @@
+// جاوااسکریپت به روشهای مختلفی می تونه دیتا(داده ها) رو نمایش بده
+// * (html) نوشتن داخل یه المان صفحه اچ تی ام ال ---> innerHTML
+// * (html) نوشتن مستقیم داخل صفحه اچ تی ام ال ---> document.write()
+// * (alert) نوشتن داخل یه پنجره هشدار ---> window.alert()
+// * (console) نوشتن داخل کنسول ---> console.log()
+
+document.getElementById("myId").innerHTML = 5 + 6;
+document.write(5 + 5);
+
+function showAlert(){
+  window.alert("yasharhabibimedia"); 
+}
+
+console.log("آموزش جاوااسکریپت");
